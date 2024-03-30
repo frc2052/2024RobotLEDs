78eb00e99d59acc3d4ab50034646de558c7a60d4
Scott Davis <scott@qonqr.com>
2024-03-30 10:38:58 -0500

    tested status modes

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,28 @@
 #include "bounce.h"
 #include "twinkle.h"
 
+#define MODE_OFF 0
+#define MODE_DANGER 1
+#define MODE_INTAKE 2
+#define MODE_HAS_NOTE 3
+#define MODE_AIMING 4
+#define MODE_AIMING_ON_TARGET 5
+#define MODE_SHOOTING 6
+#define MODE_SHOOTING_ON_TARGET 7
+#define MODE_DONE_SHOOTING 8
+#define MODE_NO_AUTO 9
+#define MODE_BLUE_AUTO 10
+#define MODE_RED_AUTO 11
+
 CRGB g_leds[NUM_LEDS]; //create our LED array object for all our LEDs
 int lastCode = -1;
 unsigned long lastScreenUpdate = 0;
 
 PinReader reader = PinReader();
-U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // Adafruit Feather ESP8266/32u4 Boards + FeatherWing OLED
+
+//always use a hardware (HW) version of the screen when possible to avoid studdering in light effects
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // Adafruit Feather ESP8266/32u4 Boards + FeatherWing OLED
+
 Pulse pulse = Pulse();
 Fire fire = Fire();
 Ice ice = Ice();
@@ -43,48 +59,64 @@ void initLightPattern(int code){
 
   switch (code)
   {
-    case 0: {
-      fill_solid(g_leds, NUM_LEDS, CRGB::White);
-      FastLED.show();
-      break;
-    }
-    case 1: {
-        pulse.init(CRGB::Green, 2);
+    case MODE_DANGER: {
+        pulse.init(CRGB::Red, 20);
         break;
     }
-    case 2: {
-        pulse.init(CRGB::Red,  10);
+    case MODE_INTAKE: {
+        pulse.init(CRGB::Blue,  10);
         break;
     }
-    case 3: {
-        fire.init();
-        break;
-    }
-    case 4: {
-        ice.init();
-        break;
-    }
-    case 5: {
+    case MODE_HAS_NOTE: {
         CRGB color = CRGB(255,50,0); //orange
         beats.init(color,color,color,color);
         break;
     }
-    case 6: {
+    case MODE_AIMING: {
+      fill_solid(g_leds, NUM_LEDS, CRGB::Yellow);
+      FastLED.show();
+      break;
+    }
+    case MODE_AIMING_ON_TARGET: {
+      fill_solid(g_leds, NUM_LEDS, CRGB::Purple);
+      FastLED.show();
+      break;
+    }
+    case MODE_SHOOTING: {
+      fill_solid(g_leds, NUM_LEDS, CRGB::White);
+      FastLED.show();
+      break;
+    }
+    case MODE_SHOOTING_ON_TARGET: {
+      fill_solid(g_leds, NUM_LEDS, CRGB::Green);
+      FastLED.show();
+      break;
+    }
+    case MODE_DONE_SHOOTING: {
+      fill_solid(g_leds, NUM_LEDS, CRGB::Red);
+      FastLED.show();
+      break;
+    }
+    case MODE_NO_AUTO: {
+        //police
         CRGB c1 = CRGB::Red;
         CRGB c2 = CRGB::Blue;
         beats.init(c1, c2, c1, c2);
         break;
     }
-    case 7: {
-      // CRGB color1 = CRGB();
-     bounce.init(CRGB::Purple, CRGB::Blue, 3, 10);
+    case MODE_BLUE_AUTO: {
+      ice.init();
       break;
     }
-    case 8: {
+    case MODE_RED_AUTO: {
+      fire.init();
+      break;
+    }
+    case 20: {
       bounce.init(CRGB::Red, CRGB::Blue, 4, 30, 15);
       break;
     }
-    case 9: {
+    case 21: {
       twinkle.init(CRGB::Yellow, 2);
     }
   }
@@ -94,33 +126,44 @@ void updateLightPattern(int code){
   switch (code)
   {
     case 0: {
-
+      FastLED.clear();
+      FastLED.show();
       break;
     }
-    case 1:
-    case 2: {
+    case MODE_DANGER:
+    case MODE_INTAKE: {
       pulse.update();    
       break;
     }
-    case 3: {
-      fire.updateSplit();
-      break;
-    }
-    case 4: {
-      ice.updateSplit();
-      break;
-    }
-    case 5:
-    case 6: {
+    case MODE_HAS_NOTE: {
       beats.update();
       break;
     }
-    case 7: 
-    case 8: {
+    case MODE_AIMING:
+    case MODE_AIMING_ON_TARGET:
+    case MODE_SHOOTING:
+    case MODE_SHOOTING_ON_TARGET:
+    case MODE_DONE_SHOOTING: {
+      //solid color - do nothing
+      break;
+    }
+    case MODE_NO_AUTO: {
+      beats.update();
+      break;
+    }
+    case MODE_BLUE_AUTO:{
+            ice.updateSplit();
+      break;
+    }
+    case MODE_RED_AUTO: {
+      fire.updateSplit();
+      break;
+    }
+    case 20: {
       bounce.update();
       break;
     }
-    case 9: {
+    case 21: {
       twinkle.update();
       break;
     }
